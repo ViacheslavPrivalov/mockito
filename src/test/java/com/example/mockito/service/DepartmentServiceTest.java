c2ebadc67a2f7c8c20735d725f7c95ac9b4ed12e
Viacheslav Privalov <privalov2306@yandex.ru>
2023-06-13 11:00:03 +0300

    Done all homework

--- src/test/java/com/example/mockito/service/DepartmentServiceTest.java
+++ src/test/java/com/example/mockito/service/DepartmentServiceTest.java
@@ -1,4 +1,76 @@
 package com.example.mockito.service;
 
+import com.example.mockito.model.Employee;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class DepartmentServiceTest {
+
+    private final EmployeeService employeeServiceMock = mock(EmployeeService.class);
+
+    private static Collection<Employee> employees = List.of(
+            new Employee("Ivan", "Ivanov", 1, 50_000.0),
+            new Employee("Ivan", "Petrov", 1, 60_000.0),
+            new Employee("Petr", "Petrov", 2, 70_000.0),
+            new Employee("Petr", "Sidorov", 2, 80_000.0)
+    );
+
+    private DepartmentServiceImpl out;
+
+    @BeforeEach
+    public void initOut() {
+        out = new DepartmentServiceImpl(employeeServiceMock);
+    }
+
+
+    @Test
+    public void shouldReturnSalary() {
+        when(employeeServiceMock.findAll()).thenReturn(employees);
+
+        assertEquals(out.maxSalary(1), 60_000);
+        assertEquals(out.maxSalary(2), 80_000);
+
+        assertEquals(out.minSalary(1), 50_000);
+        assertEquals(out.minSalary(2), 70_000);
+
+        assertEquals(out.sumSalary(1), 110_000);
+        assertEquals(out.sumSalary(2), 150_000);
+    }
+
+    @Test
+    public void shouldReturnCollection() {
+        when(employeeServiceMock.findAll()).thenReturn(employees);
+
+        Collection<Employee> expected = List.of(
+                new Employee("Ivan", "Ivanov", 1, 50_000.0),
+                new Employee("Ivan", "Petrov", 1, 60_000.0)
+        );
+
+        assertIterableEquals(expected, out.printEmployeesByDepartment(1));
+    }
+
+    @Test
+    public void shouldReturnMap() {
+        when(employeeServiceMock.findAll()).thenReturn(employees);
+
+        Map<Integer, List<Employee>> expected = new HashMap<>();
+        expected.put(1, List.of(new Employee("Ivan", "Ivanov", 1, 50_000.0),
+                new Employee("Ivan", "Petrov", 1, 60_000.0)));
+        expected.put(2, List.of(new Employee("Petr", "Petrov", 2, 70_000.0),
+                new Employee("Petr", "Sidorov", 2, 80_000.0)));
+
+        assertEquals(expected, out.printEmployees());
+    }
 }
--- src/test/java/com/example/mockito/service/EmployeeServiceTest.java
+++ src/test/java/com/example/mockito/service/EmployeeServiceTest.java
@@ -1,9 +1,74 @@
 package com.example.mockito.service;
 
+import com.example.mockito.exceptions.EmployeeAlreadyAddedException;
+import com.example.mockito.exceptions.EmployeeNotFoundException;
+import com.example.mockito.model.Employee;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EmployeeServiceTest {
     private final EmployeeService out = new EmployeeServiceImpl();
 
-    public void shouldReturnEmployee() {
+    private static Stream<Arguments> paramsForTest() {
+        return Stream.of(
+                Arguments.of("Ivan", "Ivanov", 1, 50_000.0),
+                Arguments.of("Petr", "Petrov", 1, 60_000.0),
+                Arguments.of("Dima", "Ivanov", 2, 70_000.0),
+                Arguments.of("Dima", "Petrov", 2, 80_000.0),
+                Arguments.of("", "", 1, 0.0),
+                Arguments.of("  ", "  ", 100, 1_000_000.0)
+        );
+    }
 
+    @ParameterizedTest
+    @MethodSource("paramsForTest")
+    public void shouldReturnEmployee(String firstName, String lastName, Integer id, Double salary) {
+        Employee expected = new Employee(firstName, lastName, id, salary);
+
+        Employee actual1 = out.addEmployee(firstName, lastName, id, salary);
+        Employee actual2 = out.findEmployee(firstName, lastName);
+        Employee actual3 = out.removeEmployee(firstName, lastName);
+
+        assertEquals(actual1, expected);
+        assertEquals(actual2, expected);
+        assertEquals(actual3, expected);
+    }
+
+    @Test
+    public void shouldReturnCollection() {
+        Employee employee1 = new Employee("Ivan", "Ivanov", 1, 50_000.0);
+        Employee employee2 = new Employee("Ivan", "Petrov", 1, 60_000.0);
+        Collection<Employee> expected = new ArrayList<>();
+        expected.add(employee1);
+        expected.add(employee2);
+
+        out.addEmployee("Ivan", "Ivanov", 1, 50_000.0);
+        out.addEmployee("Ivan", "Petrov", 1, 60_000.0);
+        Collection<Employee> actual = out.findAll();
+
+        assertIterableEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowEmployeeAlreadyAddedException() {
+        out.addEmployee("Ivan", "Ivanov", 1, 50_000.0);
+        assertThrows(EmployeeAlreadyAddedException.class,
+                () -> out.addEmployee("Ivan", "Ivanov", 1, 50_000.0));
+    }
+
+    @Test
+    public void shouldThrowEmployeeNotFoundException() {
+        assertThrows(EmployeeNotFoundException.class,
+                () -> out.findEmployee("Ivan", "Ivanov"));
+        assertThrows(EmployeeNotFoundException.class,
+                () -> out.removeEmployee("Ivan", "Ivanov"));
     }
 }
